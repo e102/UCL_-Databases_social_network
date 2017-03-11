cd0dbf0de417d76716cee38a77aa5f4e1cf405d1
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-03-11 15:16:33 +0000

    misc styling

--- add_friend.php
+++ add_friend.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
-include ("includes/connection.php");
-include ("functions/functions.php");
-include ("template/Main/header.php");
+include("includes/connection.php");
+include("functions/functions.php");
+include("template/Main/header.php");
 
-if(!isset($_SESSION['user_email'])){
+if (!isset($_SESSION['user_email'])) {
     header("location: index.php");
-} else {
+}
+else {
 
     ?>
 
     <div class="content">
         <!-- user timeline starts -->
         <div id="user_timeline">
-            <?php include ("template/Main/user_details.php");?>
+            <?php include("template/Main/user_details.php"); ?>
         </div>
         <!-- user timeline ends -->
         <!-- content timeline starts -->
@@ -22,28 +23,22 @@ if(!isset($_SESSION['user_email'])){
             <div id="posts">
 
                 <?php
-
-                if(isset($_GET['u_id'])) {
+                if (isset($_GET['u_id'])) {
 
                     $id = $_GET['u_id'];
 
                     $user = $_SESSION['user_email'];
                     $get_user = "select * from users where user_email = '$user'";
-                    $run_user = mysqli_query($con,$get_user);
+                    $run_user = mysqli_query($con, $get_user);
                     $row = mysqli_fetch_array($run_user);
                     $user_id = $row['user_id'];
 
                     echo "<h3>Friend request sent</h3>";
                     $insert = "insert into friends (requestSenderID, requestReceiverID, verified) values ('$user_id','$id','no')";
 
-                    $run = mysqli_query($con,$insert);
+                    $run = mysqli_query($con, $insert);
 
                 }
-
-
-
-
-
                 ?>
             </div>
         </div>
