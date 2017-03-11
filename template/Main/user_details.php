349c4adcaeb7f9dcfeb0a353e81d07c47d9da691
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-03-11 15:03:13 +0000

    user_details correctly handles 0 friend requests

--- template/Main/user_details.php
+++ template/Main/user_details.php
@@ -1,29 +1,34 @@
-
 <div id="user_details">
-                <?php
-                $user = $_SESSION['user_email'];
-                $get_user = "select * from users where user_email = '$user'";
-                $run_user = mysqli_query($con, $get_user);
-                $row = mysqli_fetch_array($run_user);
-                $user_id = $row['user_id'];
-                $user_name = $row['user_name'];
-                $user_pass = $row['user_pass'];
-                $user_email = $row['user_email'];
-                $user_image = $row['user_image'];
-                $user_country = $row['user_country'];
-                $user_gender = $row['user_gender'];
-                $register_date = $row['register_date'];
-                $last_login = $row['last_login'];
+    <?php
+    $user = $_SESSION['user_email'];
+    $get_user = "select * from users where user_email = '$user'";
+    $run_user = mysqli_query($con, $get_user);
+    $row = mysqli_fetch_array($run_user);
+    $user_id = $row['user_id'];
+    $user_name = $row['user_name'];
+    $user_pass = $row['user_pass'];
+    $user_email = $row['user_email'];
+    $user_image = $row['user_image'];
+    $user_country = $row['user_country'];
+    $user_gender = $row['user_gender'];
+    $register_date = $row['register_date'];
+    $last_login = $row['last_login'];
 
-                $user_posts = "select * from posts where user_id='$user_id'";
-                $run_posts = mysqli_query($con,$user_posts);
-                $numberPosts = mysqli_num_rows($run_posts);
+    $user_posts = "select * from posts where user_id='$user_id'";
+    $run_posts = mysqli_query($con, $user_posts);
+    $numberPosts = mysqli_num_rows($run_posts);
 
-                $friend_requests = "select * from friends where requestReceiverID='$user_id' and verified='no'";
-                $run_requests = mysqli_query($con,$friend_requests);
-                $numberRequests = mysqli_num_rows($run_requests);
+    $friend_requests = "select * from friends where requestReceiverID='$user_id' and verified='no'";
+    $run_requests = mysqli_query($con, $friend_requests);
+    if ($run_requests == null) {
+        $numberRequests = 0;
+    }
+    else {
+        $numberRequests = mysqli_num_rows($run_requests);
+    }
 
-                echo "
+
+    echo "
                       <center><img src='user/user_images/$user_image' width='200px' height='200px'/></center>
                       <p><strong>Name: $user_name</strong></p>
                       <p><strong>Country: $user_country</strong></p>
@@ -37,5 +42,5 @@
                       <p><a href='friend_requests.php?u_id=$user_id'>Friend requests ($numberRequests)</a></p>
                       <p><a href='logout.php'>Logout</a></p>
                       ";
-                ?>
+    ?>
 </div>
