e50a0e11067822b9b2eab4fa420b373cc41af305
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-03-13 15:42:31 +0000

    Rationalized code
    Moved userProfile from functions to user page

--- functions/functions.php
+++ functions/functions.php
@@ -83,51 +83,6 @@ function insertPost()
     }
 }
 
-function userProfile(){
-    global $con;
-
-    if (isset($_GET['u_id'])) {
-
-        $user_id = $_GET['u_id'];
-
-        $select = "select * from users where user_id='$user_id'";
-        $run = mysqli_query($con, $select);
-        $row = mysqli_fetch_array($run);
-
-        $id = $row['user_id'];
-        $country = $row['user_country'];
-        $name = $row['user_name'];
-        $image = $row['user_image'];
-        $gender = $row['user_gender'];
-        $last_login = $row['last_login'];
-        $register_date = $row['register_date'];
-
-        if ($gender == 'Male') {
-            $msg = "Send him a message";
-        }
-        else {
-            $msg = "Send her a message";
-        }
-
-        echo "
-        <div id='user_profile'>
-
-        <p><img src='user/user_images/$image' width='50' height='50'></p><br/>
-        <p><strong>Name: $name</strong></p><br/>
-        <p><strong>Name: $gender</strong></p><br/>
-        <p><strong>Country: $country</strong></p><br/>
-        <p><strong>Last Login: $last_login</strong></p><br/>
-        <p><strong>Member Since: $register_date</strong></p><br/>
-        <a href='messages.php?u_id=$id'><button>$msg</button></a>
-        <a href='add_friend.php?u_id=$id'><button>Send friend request</button></a>
-        </div>
-        ";
-
-
-    }
-
-}
-
 function userPosts()
 {
     global $con;
