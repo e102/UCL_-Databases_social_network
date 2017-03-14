de9d8af36705895a082e2cc65108a52dfe77cc04
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-03-14 14:28:27 +0000

    Bugfix: Users can update country/gender

--- functions/user_update.php
+++ functions/user_update.php
@@ -7,13 +7,15 @@ if (isset($_POST['update'])) {
     $name = $_POST['u_name'];
     $pass = $_POST['u_pass'];
     $email = $_POST['u_email'];
+    $country = $_POST['u_country'];
+    $user_gender = $_POST['u_gender'];
     $image = $_FILES['u_image']['name'];
     $image_tmp = $_FILES['u_image']['tmp_name'];
     $profile_is_private = $_POST['profile_is_private'];
 
     move_uploaded_file($image_tmp, "user/user_images/$image");
 
-    $update = "update users set user_name='$name',user_pass='$pass',user_email='$email',user_image='$image', profile_is_private = '$profile_is_private' where user_id='$user_id'";
+    $update = "update users set user_name='$name',user_pass='$pass',user_email='$email',user_country = '$country',user_gender='$user_gender',user_image='$image', profile_is_private = '$profile_is_private' where user_id='$user_id'";
     $run_update = mysqli_query($con, $update);
 
     if ($run_update) {
