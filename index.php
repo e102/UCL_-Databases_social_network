be54578bfc9f78a78332c81a310d2ee995ac4d29
johnnyh01 <john89hill@gmail.com>
2017-03-14 13:28:28 +0000

    Styled home page

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include("functions/functions.php");
 include("functions/user_insert.php");
 include("template/Login/header.php");
 include("template/Login/content.php");
-include("template/Login/footer.php");
 include("login.php");
 ?>
 
--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
-session_start();
+
+if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+
 include("includes/connection.php");
 if (isset($_POST['login'])) {
 
