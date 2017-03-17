d41cc2cbee4d2ca84c43c74e49c4e70722f6f682
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-03-17 18:12:08 +0000

    Formatting

--- user_profile.php
+++ user_profile.php
@@ -18,51 +18,52 @@ if (!isset($_SESSION['user_email'])) {
 }
 else {
 
-    ?>
+?>
 
-    <div class="content">
-        <!-- user timeline starts -->
-        <div id="user_timeline">
-            <?php include("template/Main/user_details.php"); ?>
-        </div>
-        <!-- user timeline ends -->
-        <!-- content timeline starts -->
-        <div id="content_timeline">
-
-            <div id="posts">
-
-                <?php
-
-                $u_id = $_GET['u_id'];
-
-                $user = $_SESSION['user_email'];
-                $get_user = "select * from users where user_email = '$user'";
-                $run_user = mysqli_query($con,$get_user);
-                $row = mysqli_fetch_array($run_user);
-                $user_id = $row['user_id'];
-
-                $select = "select * from friends where verified='yes' and requestSenderID='$user_id' and requestReceiverID='$u_id'";
-                $run = mysqli_query($con,$select);
-
-                $row_user = mysqli_fetch_array($run);
-
-                if ($row_user){
-                    include ("functions/friend_posts.php");
-                } else {
-                    userProfile();
-                }
-
-                ?>
-            </div>
-        </div>
-        <!-- content timeline ends -->
+<div class="content">
+    <!-- user timeline starts -->
+    <div id="user_timeline">
+        <?php include("template/Main/user_details.php"); ?>
     </div>
-    <!-- content area ends -->
-    </div>
-    <!-- container ends -->
+    <!-- user timeline ends -->
+    <!-- content timeline starts -->
+    <div id="content_timeline">
 
-    </body>
-    </html>
+        <div id="posts">
+
+            <?php
+
+            $u_id = $_GET['u_id'];
+
+            $user = $_SESSION['user_email'];
+            $get_user = "select * from users where user_email = '$user'";
+            $run_user = mysqli_query($con, $get_user);
+            $row = mysqli_fetch_array($run_user);
+            $user_id = $row['user_id'];
+
+            $select = "select * from friends where verified='yes' and requestSenderID='$user_id' and requestReceiverID='$u_id'";
+            $run = mysqli_query($con, $select);
+
+            $row_user = mysqli_fetch_array($run);
+
+            if ($row_user) {
+                include("functions/friend_posts.php");
+            }
+            else {
+                userProfile();
+            }
+
+            ?>
+        </div>
+    </div>
+    <!-- content timeline ends -->
+</div>
+<!-- content area ends -->
+</div>
+<!-- container ends -->
+
+</body>
+</html>
 <?php } ?>
 
 <?php
@@ -108,4 +109,5 @@ function userProfile()
 ";
     }
 }
+
 ?>
