27112f652bd69685bc3c71b6a9b09084d86eb492
Louis Fillo <louisfillo@yahoo.com>
2017-03-17 17:10:01 +0000

    Blog attempted fix

--- functions/functions.php
+++ functions/functions.php
@@ -311,36 +311,38 @@ function getBlogs() {
       global $user_id;
 
       $blog_query = $_GET['user_query'];
-      $get_friends = "SELECT * FROM friends WHERE requestSenderID = '$user_id'";
+
+      $get_friends = "SELECT * FROM friends WHERE requestSenderID = '$user_id' and verified = 'yes'";
       $run_friends = mysqli_query($con, $get_friends);
-      $row_friends = mysqli_fetch_array($run_friends);
-      $friend_id = $row_friends['requestReceiverID'];
 
-      $get_blog = "SELECT * FROM posts WHERE post_title LIKE '%$blog_query'";
-      $run_blog = mysqli_query($con, $get_blog);
+      while ($row_friends = mysqli_fetch_array($run_friends)) {
+            echo "<script>alert('sajhfg')</script>";
+          $friend_id = $row_friends['requestReceiverID'];
 
+          $get_blog = "SELECT * FROM posts WHERE post_title LIKE '%$blog_query' and user_id = '$friend_id'";
+          $run_blog = mysqli_query($con, $get_blog);
 
-      while ($row_blog = mysqli_fetch_array($run_blog)) {
+          while ($row_blog = mysqli_fetch_array($run_blog)) {
 
-        $post_id = $row_blog['post_id'];
-        $user_id = $row_blog['user_id'];
-        $post_title = $row_blog['post_title'];
-        $content = $row_blog['post_content'];
-        $post_date = $row_blog['post_date'];
-        $photo_path = $row_blog['post_photo_path'];
+              $post_id = $row_blog['post_id'];
+              $user_id = $row_blog['user_id'];
+              $post_title = $row_blog['post_title'];
+              $content = $row_blog['post_content'];
+              $post_date = $row_blog['post_date'];
+              $photo_path = $row_blog['post_photo_path'];
 
-        $user = "select * from users where user_id='$user_id' and posts='yes'";
-        $run_user = mysqli_query($con, $user);
-        $row_user = mysqli_fetch_array($run_user);
-        $user_name = $row_user['user_name'];
-        $user_image = $row_user['user_image'];
+              $user = "select * from users where user_id='$user_id' and posts='yes'";
+              $run_user = mysqli_query($con, $user);
+              $row_user = mysqli_fetch_array($run_user);
+              $user_name = $row_user['user_name'];
+              $user_image = $row_user['user_image'];
 
-        if ($user_id == $friend_id){
-          echo "<div id='posts' style='width: 480px; margin-bottom:10px'>";
-          if ($photo_path) {
-              echo "<p><img src = 'user/user_images/$photo_path' width = '50' height = '50' ></p >";
-          }
-          echo "<h3><a href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+              if ($user_id == $friend_id) {
+                  echo "<div id='posts' style='width: 480px; margin-bottom:10px'>";
+                  if ($photo_path) {
+                      echo "<p><img src = 'user/user_images/$photo_path' width = '50' height = '50' ></p >";
+                  }
+                  echo "<h3><a href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
               <h3>$post_title</h3>
               <p>$post_date</p>
               <p>$content</p>
@@ -349,10 +351,10 @@ function getBlogs() {
                   <button class='btn-white btn-small' >View</button> </a>
               </div>
               </div>";
-        }
-        else {
-          echo "<h2>No posts found</h2>";
-        }
+              } else {
+                  echo "<h2>No posts found</h2>";
+              }
+          }
       }
   }
 }
