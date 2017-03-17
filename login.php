f5a8a8f701f2d888386699d5df8b1d1464fde5be
Louis Fillo <louisfillo@yahoo.com>
2017-03-17 13:19:40 +0000

    Added delete post functionality

--- login.php
+++ login.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION))
 include("includes/connection.php");
 if (isset($_POST['login'])) {
 
+    $_SESSION['user_email'] = $_POST['email'];
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $pass = mysqli_real_escape_string($con, $_POST['pass']);
 
