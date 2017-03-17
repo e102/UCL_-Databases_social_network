ffafddb718eefc2a9281745568bc82b0e6ef3781
Srdjan Miletic <srdjanmiletic2@gmail.com>
2017-03-17 17:04:26 +0000

    Can upload photos to albums

--- functions/single_album.php
+++ functions/single_album.php
@@ -1,3 +1,4 @@
+
 <?php
 include("includes/connection.php");
 if (isset($_GET['album_id'])) {
@@ -16,7 +17,7 @@ if (isset($_GET['album_id'])) {
     $user_name = $row_user['user_name'];
     $user_image = $row_user['user_image'];
 
-    echo"<br>";
+    echo "<br>";
 
     echo "
     <div id='posts'>
@@ -24,9 +25,31 @@ if (isset($_GET['album_id'])) {
     <h3><a href='user_profile.php?u_id=$album_owner_id'>Album Owner: $user_name</a></h3>
     <h3>Album Name: $album_name</h3>";
 
+
+    //Album image adder
+    $get_user_photos = "select * from photos where photo_owner = '$album_owner_id'";
+    $run_user_photos = mysqli_query($con, $get_user_photos);
+    $user_photos_array = array();
+    while ($row_user_photos = mysqli_fetch_array($run_user_photos)) {
+        $user_photo_id = $row_user_photos['photo_id'];
+        $user_photos_array[] = $user_photo_id;
+    }
+
+    echo "<h3>Add an image</h3>";
+    echo"<form action='' method='post'>";
+    echo "<select name='select_photo' id='country_dropdown_box' style='width: auto;''>";
+    foreach ($user_photos_array as $a_photo) {
+        echo "<option value='$a_photo'>$a_photo</option>";
+    }
+    echo "</select>";
+    echo "<button name='upload_photo' class='btn-white btn-small'>Upload Photo</button>";
+    echo"</form>";
+    echo "<br>";
+
+
     $photos_query = "select * from photo_album_contents where album_id='$album_id'";
     $run_photos = mysqli_query($con, $photos_query);
-    while($row_photos = mysqli_fetch_array($run_photos)){
+    while ($row_photos = mysqli_fetch_array($run_photos)) {
         $single_photo_id = $row_photos['photo_id'];
         echo "<script>console.log('photo id = ' + $single_photo_id)</script>";
         $get_single_photo = "select * from photos where photo_id = '$single_photo_id'";
@@ -36,14 +59,17 @@ if (isset($_GET['album_id'])) {
         $single_photo_path = $single_photo_row['photo_path'];
 
         echo "<p><img src='user/user_images/$single_photo_path' width='50' height='50'></p>";
-        echo"</br>";
+        echo "</br>";
     }
 }
 
-function add_photo($photo_id, $album_id){
+if (isset($_POST['upload_photo'])) {
     global $con;
-    $insert_photo = "insert into photo_album_contents (photo_id,album_id) VALUES ($photo_id,$album_id)";
-    $run_insert = sqlite_query($con, $insert_photo);
+    $photo_id = $_POST['select_photo'];
+    $album_id = $_GET['album_id'];
+    $insert_photo = "insert into photo_album_contents (photo_id,album_id) VALUES ('$photo_id','$album_id')";
+    echo "<script>console.log($insert_photo)</script>";
+    $run_insert = mysqli_query($con, $insert_photo);
     if ($run_insert) {
         echo "<script>alert('Photo added to album')</script>";
     }
